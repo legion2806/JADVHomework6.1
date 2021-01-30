7b8aa735cd4ecb97eb92a1a656deaabfac5774e4
legion2806 <benito2856@gmail.com>
2021-01-30 18:22:47 +0200

    Initial commit Number4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,16 +9,20 @@ public class Main {
         String aborigens = "jfhfhjd jfhfhjd jdhhosjid jdhhosjid fjdhfsjhfks fjdhfsjhfks " +
                 "djnsdmkma dsfnp djngkmvxgk ko";
 
-        Set<String> toFilter = Set.of("jfhfhjd", "jdhhosjid", "fjdhfsjhfks");
-        Set<String> seen = new HashSet<>();
+        String[] element = new String[1];
 
         System.out.println("Ваш словарь:");
 
         // Монада
         Arrays.stream(aborigens.split("\\s"))
                 .sorted()
-                .filter(s -> !toFilter.contains(s) || seen.add(s))
-                .map(w -> w + " - это слово ещё неизвестно!")
+                .filter(x -> {
+                    if (!x.equalsIgnoreCase(element[0])) {
+                        element[0] = x;
+                        return true;
+                    }
+                    return false;
+                }).map(w -> w + " - это слово ещё неизвестно!")
                 .forEach(System.out::println);
 
     }
